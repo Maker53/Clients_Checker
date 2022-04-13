5d97bde9bcd24705e9cbdbf02d57e85ce1ef2c65
Shalgin Stanislav <s.shalgin@gmail.com>
2022-04-13 20:34:53 +0300

    feat(ClientCell): add custom cell class

---
+++ Check_Clients/ClientList/View/ClientCell.swift
@@ -0,0 +1,12 @@
+//
+//  ClientCell.swift
+//  Check_Clients
+//
+//  Created by Станислав on 13.04.2022.
+//
+
+import UIKit
+
+class ClientCell: UITableViewCell {
+    static let identifier = String(describing: ClientCell.self)
+}
