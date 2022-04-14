89a2c0feaf9f013f0b824cbdbdc9e90bc7b7803b
Shalgin Stanislav <s.shalgin@gmail.com>
2022-04-14 11:13:58 +0300

    feat(SetupNewClientViewController): add text fields for input client data

--- New Client/Controller/Extensions/SetupNewClientViewController.swift
+++ New Client/Controller/Extensions/SetupNewClientViewController.swift
@@ -25,5 +25,36 @@ extension NewClientViewController {
         view.backgroundColor = .systemBackground
         navigationItem.leftBarButtonItem = cancelBarButton
         navigationItem.rightBarButtonItem = saveBarButton
+        
+        setupTextFields()
+    }
+    
+    private func setupTextFields() {
+        let clientNameTF = UITextField()
+        let locationTF = UITextField()
+        let visitTimeTF = UITextField()
+        let stackView = UIStackView(arrangedSubviews: [clientNameTF, locationTF, visitTimeTF])
+        
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 25
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        clientNameTF.placeholder = "Client name"
+        clientNameTF.borderStyle = .roundedRect
+        locationTF.placeholder = "Location"
+        locationTF.borderStyle = .roundedRect
+        visitTimeTF.placeholder = "Visit time"
+        visitTimeTF.borderStyle = .roundedRect
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            stackView.heightAnchor.constraint(equalToConstant: 165)
+        ])
     }
 }
