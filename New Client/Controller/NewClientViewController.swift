f4732e714ec84f0c16e9e538c9f4a755bf5bc983
Shalgin Stanislav <s.shalgin@gmail.com>
2022-04-14 12:57:18 +0300

    feat(NewClientViewController): add properties for UI elements

--- New Client/Controller/NewClientViewController.swift
+++ New Client/Controller/NewClientViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class NewClientViewController: UIViewController {
     
+    // MARK: - Public Properties
+    lazy var cancelBarButton = UIBarButtonItem()
+    lazy var saveBarButton = UIBarButtonItem()
+    lazy var clientNameTF = UITextField()
+    lazy var locationTF = UITextField()
+    lazy var visitTimeTF = UITextField()
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        view.endEditing(true)
+    }
+    
+    // MARK: - Buttons action
     @objc func cancelBarButtonTapped() {
         dismiss(animated: true)
     }
