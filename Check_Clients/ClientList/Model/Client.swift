da54290f799146753ab070c503d4d6267d0f3c45
Shalgin Stanislav <s.shalgin@gmail.com>
2022-04-14 09:25:47 +0300

    feat(Client): add Client model

---
+++ Check_Clients/ClientList/Model/Client.swift
@@ -0,0 +1,14 @@
+//
+//  Client.swift
+//  Check_Clients
+//
+//  Created by Станислав on 14.04.2022.
+//
+
+import Foundation
+
+struct Client {
+    var clientName: String
+    var location: String
+    var visitTime: Date
+}
