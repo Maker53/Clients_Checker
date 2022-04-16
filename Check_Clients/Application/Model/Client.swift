adfffe6756c008dc9d50775e57e00a135ca67889
Shalgin Stanislav <s.shalgin@gmail.com>
2022-04-16 10:03:51 +0300

    refactor(Client): refactor model for the realm

---
+++ Check_Clients/Application/Model/Client.swift
@@ -0,0 +1,27 @@
+//
+//  Client.swift
+//  Check_Clients
+//
+//  Created by Станислав on 14.04.2022.
+//
+
+import RealmSwift
+
+class Client: Object {
+    @Persisted var clientName: String
+    @Persisted var location: String
+    @Persisted var visitTime: Date
+    // TODO: Добавить свойство с типом бул, которое будет отображать оконченные сессии
+    
+    init(clientName: String, location: String, visitTime: Date) {
+        self.clientName = clientName
+        self.location = location
+        self.visitTime = visitTime
+    }
+    
+    override init() {
+        self.clientName = ""
+        self.location = ""
+        self.visitTime = Date()
+    }
+}
