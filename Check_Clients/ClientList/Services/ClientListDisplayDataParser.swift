5878b3209d84b06e98425b4e3520a5e6ed40a1c3
Shalgin Stanislav <s.shalgin@gmail.com>
2022-04-17 09:38:08 +0300

    feat(ClientListDisplayDataParser): add parser

---
+++ Check_Clients/ClientList/Services/ClientListDisplayDataParser.swift
@@ -0,0 +1,12 @@
+//
+//  ClientListDisplayDataParser.swift
+//  Check_Clients
+//
+//  Created by Станислав on 17.04.2022.
+//
+
+class ClientListDisplayDataParser {
+    static let shared = ClientListDisplayDataParser()
+    
+    private init() {}
+}
